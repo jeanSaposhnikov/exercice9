fee6c4625468e5cc2d4b218bfacd813a7fb1fe14
Saposhnikov <jean.saposhnikov@hotmail.fr>
2020-03-03 18:04:53 -0500

    Enlever commentaires

--- category-evenements.php
+++ category-evenements.php
@@ -27,17 +27,6 @@ get_header();
 				$gridArea = $jour . '/' . $mois . '/' . ($jour+1) . '/' . ($mois+1);
 				// echo '<p>' .$gridArea . '</p>' ;
 				echo '<h2  style="grid-area:'. $gridArea . '">' . get_the_title() . $gridArea .get_the_date(' j / m / Y') .'</h2>';
-				
-
-                //echo '<div>' . get_the_title($query->post->ID) . '</div>';
-               
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-                //get_template_part( 'template-parts/content', get_post_type() );
-                
 			endwhile;
 		?>
         </section>
